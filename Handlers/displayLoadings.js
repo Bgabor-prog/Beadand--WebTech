a46659a0b2a5bcd8301646caadda6a66563d54b9
Gabor Bartok-Balogh <gabor.bartok-balogh.ext@evosoft.com>
2022-10-07 12:17:36 +0200

    teszt

---
+++ Handlers/displayLoadings.js
@@ -0,0 +1,4 @@
+function load_to_display() {
+    document.getElementById("display").innerHTML='<object type="text/html" data="history.html" style="height:100%;width:100%"></object>';
+    document.getElementById("display").style.opacity = "1";
+}
