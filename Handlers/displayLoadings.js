18986037239cdcd07fa5d8560f6d34a1806f4689
Bgabor-prog <demiteto.bartok@gmail.com>
2022-10-11 14:22:21 +0200

    Updated

--- Handlers/displayLoadings.js
+++ Handlers/displayLoadings.js
@@ -1,4 +1,23 @@
-function load_to_display() {
+function load_History() {
+    setTimeout(function(){
     document.getElementById("display").innerHTML='<object type="text/html" data="history.html" style="height:100%;width:100%"></object>';
-    document.getElementById("display").style.opacity = "1";
+    document.getElementById("display").style.opacity = "1"},1200);
+}
+
+function load_Home(){
+    setTimeout(function(){
+    document.getElementById("display").innerHTML='<object type="text/html" data="homepage.html" style="height:100%;width:100%"></object>';
+    document.getElementById("display").style.opacity = "1"},1200);
+}
+
+function load_Games(){
+    setTimeout(function(){
+    document.getElementById("display").innerHTML='<object type="text/html" data="games.html" style="height:100%;width:100%"></object>';
+    document.getElementById("display").style.opacity = "1"},1200);
+}
+
+function load_Prices(){
+    setTimeout(function(){
+    document.getElementById("display").innerHTML='<object type="text/html" data="prices.html" style="height:100%;width:100%;text-align:center"></object>';
+    document.getElementById("display").style.opacity = "1"},1200);
 }
